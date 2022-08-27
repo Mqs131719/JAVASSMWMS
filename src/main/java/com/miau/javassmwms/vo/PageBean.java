04fdbcb0b5a84df121a263ef60647b97366e96b5
Miau <15537559926@163.com>
2022-08-27 19:35:53 +0800

    第一版架构

---
+++ src/main/java/com/miau/javassmwms/vo/PageBean.java
@@ -0,0 +1,54 @@
+package com.miau.javassmwms.vo;
+
+import com.feri.fengstudy.config.SystemConfig;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　 ┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　  ┃
+ * 　　┃　　　　　　 ┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛Code is far away from bug with the animal protecting
+ * 　　　　┃　　　┃    神兽保佑,代码无bug
+ * 　　　　┃　　　┃
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━━━永无BUG!━━━━━━
+ *
+ * @Description:
+ * @Author: 邢朋辉
+ * @Date: 2022/8/27 11:16
+ */
+@Data
+@NoArgsConstructor
+public class PageBean {
+    private int code;
+    private String msg;
+    private long count;
+    private List data;
+
+    /**
+     * 成功 返回
+     * @param count 总条数
+     * @param data 本页数据内容*/
+    public static PageBean ok(long count,List data){
+        PageBean bean=new PageBean();
+        bean.setCode(SystemConfig.PAGE_OK);
+        bean.setMsg("OK");
+        bean.setCount(count);
+        bean.setData(data);
+        return bean;
+    }
+}
---
+++ src/main/java/com/miau/javassmwms/vo/R.java
@@ -0,0 +1,63 @@
+package com.miau.javassmwms.vo;
+
+import com.feri.fengstudy.config.SystemConfig;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　 ┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　  ┃
+ * 　　┃　　　　　　 ┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛Code is far away from bug with the animal protecting
+ * 　　　　┃　　　┃    神兽保佑,代码无bug
+ * 　　　　┃　　　┃
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━━━永无BUG!━━━━━━
+ *
+ * @Description:
+ * @Author: 邢朋辉
+ * @Date: 2022/8/27 11:11
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class R {
+    private int code;
+    private String msg;
+    private Object data;
+
+    /**
+     * 成功*/
+    public static R ok(Object data){
+        //魔法值：直接写值的(数字、字符串等)，开发中应该避免魔法值，要么用枚举要么用常量
+//        return new R(200,"OK",data);
+        return new R(SystemConfig.R_OK,"OK",data);
+    }
+    /**
+     * 成功*/
+    public static R ok(){
+        return ok(null);
+    }
+    /**
+     * 失败*/
+    public static R fail(String msg){
+        return new R(SystemConfig.R_FAIL,msg,null);
+    }
+    /**
+     * 失败*/
+    public static R fail(){
+        return fail("FAIL");
+    }
+}
