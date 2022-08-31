74442851d7961a4accb1e6630adfe0167f51ae0c
马启胜 <1316093893@qq.com>
2022-08-31 16:08:27 +0800

    有问题

--- src/main/java/com/miau/javassmwms/service/intf/InventoryService.java
+++
@@ -1,7 +0,0 @@
-//package com.miau.javassmwms.service.intf;
-//
-//import com.miau.javassmwms.vo.PageBean;
-//
-//public interface InventoryService {
-//    PageBean queryPage(int page, int size);
-//}
